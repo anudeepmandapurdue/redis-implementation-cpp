7439bf90b02146a6691eb4974a0dad5367781ee3
anudeepmandapurdue <anudeepsm13@gmail.com>
2026-03-02 17:46:23 -0500

    feat: first commit. so far: build a basic TCP/IP connection with a server and client that can read and write to each other. next update was to be able to send text all at once instead of in sections and bytes. for example hello would not become he ll o

---
+++ src/client.cpp
@@ -0,0 +1,140 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <cassert>
+
+//structs already defined in headers, here for understanding what is in them and the different varaiables. 
+/*
+struct sockaddr_in {
+    uint16_t sin_family;
+    uint16_t sin_port;
+    struct in_addr sin_addr;
+};
+
+struct in_addr{
+    uint32_t s_addr;
+};
+*/
+int32_t k_max_msg = 4096;
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static int32_t read_full(int fd, char *buf, size_t n ){
+    while (n>0){
+        ssize_t rv = read(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert((size_t)rv <= n );
+        n -= (size_t)rv;
+        buf += rv;
+        
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, char *buf, size_t n){
+    while (n >0){
+        ssize_t rv = write(fd, buf, n );
+
+    
+     assert((size_t)rv <= n );
+        n -= (size_t)rv;
+        buf += rv;
+        
+    }
+    return 0;
+}
+
+static int32_t query(int fd, const char *text){
+    //get message size from server
+    uint32_t len = (uint32_t)strlen(text);
+    //make sure the message is less than max
+    if (len > k_max_msg){
+        return -1;
+    }
+
+    //send request
+    char wbuf[4+ k_max_msg]; //create write buffer excluding the 4 byte header
+    memcpy(wbuf, &len, 4); // header
+    memcpy(&wbuf[4], text, len); //text
+    if (int32_t err = write_all(fd, wbuf, 4 + len)) { // write to the socket handle of the server with the text and 
+        return err;
+    }
+
+    //exclude 4 bytes header
+    char rbuf[4+ k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if(err){
+        msg(errno == 0? "EOF" : "read() error");
+        return -1;
+    }
+
+    memcpy(&len, rbuf, 4);
+    if(len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    //reply body
+    err = read_full(fd, &rbuf[4], len);
+    if(err){
+        msg("read() error");
+        return err;
+    }
+
+    //print server response
+    printf("server says: %.*s\n", len, &rbuf[4]);
+    return 0;
+
+    //wbuf: [length][hello server]
+
+
+}
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
+
+    // multiple requests
+    int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;
+    }
+
+L_DONE:
+    close(fd);
+    return 0;
+}
